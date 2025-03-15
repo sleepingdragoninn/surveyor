355f973d659ae13b541971793e33619b3579d1ac
sisby-folk <nora.herrah.heartstrings@gmail.com>
2025-03-16 09:20:57 +1100

    safer splitset

--- src/main/java/folk/sisby/surveyor/util/ListUtil.java
+++ src/main/java/folk/sisby/surveyor/util/ListUtil.java
@@ -5,12 +5,9 @@ import java.util.BitSet;
 import java.util.List;
 
 public class ListUtil {
-
 	public static <T> List<T> splitSet(List<T> list, BitSet set, BitSet oldSet) {
 		List<T> outList = new ArrayList<>();
-		
-		int length = Math.min(Math.min(oldSet.length(), set.length()), list.size());
-		for (int i = 0; i < length; i++) {
+		for (int i = 0; i < Math.min(oldSet.length(), set.length()); i++) {
 			if (set.get(i)) outList.add(list.get(i));
 		}
 		return outList;
